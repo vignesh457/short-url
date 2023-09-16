360d6d26c913397e6395ed3a97c136fe09a7651c
vignesh <donapativigneshwar@gmail.com>
2023-09-16 15:32:42 +0530

    added ignore /favicon.ico

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
 app.use(express.static(path.resolve("view")));
+app.use('/favicon.ico', (req, res) => {
+    res.status(204).end();
+});
 
 app.set('view engine', 'ejs');
 app.set("views", path.resolve("view"))
